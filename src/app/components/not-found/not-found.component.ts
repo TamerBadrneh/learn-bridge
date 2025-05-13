fa3a33d4ebb50fef8d9e62098fd29279f0ea005a
Tamer <tamerse21business@gmail.com>
2025-05-14 00:41:00 +0300

    Add Card Logic Changed: No dependency on local Storage again

--- src/app/components/not-found/not-found.component.ts
+++ src/app/components/not-found/not-found.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-not-found',
   standalone: false,
   templateUrl: './not-found.component.html',
-  styleUrl: './not-found.component.scss'
+  styleUrl: './not-found.component.scss',
 })
 export class NotFoundComponent {
-
-  constructor(private _Router: Router) {
-
-  }
+  constructor(private _Router: Router, private authService: AuthService) {}
 
   navigateToHome() {
-    this._Router.navigate(['/home']);
+    this._Router.navigate([
+      `${this.authService.userData.role.toLowerCase() || ''}/home`,
+    ]);
   }
 }
